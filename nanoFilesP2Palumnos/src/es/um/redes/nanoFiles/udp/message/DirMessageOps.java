442056f53ecc22f58c3d735ec516eb4994672487
Nicole <nicole.munoz.mahecha@gmail.com>
2026-02-03 17:32:27 +0100

    zip del av con codigo inicial

---
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
@@ -0,0 +1,17 @@
+package es.um.redes.nanoFiles.udp.message;
+
+public class DirMessageOps {
+
+	/*
+	 * TODO: (Boletín MensajesASCII) Añadir aquí todas las constantes que definen
+	 * los diferentes tipos de mensajes del protocolo de comunicación con el
+	 * directorio (valores posibles del campo "operation").
+	 */
+	public static final String OPERATION_INVALID = "invalid_operation";
+	public static final String OPERATION_PING = "ping";
+	// TODO: definir las operaciones del protocolo de directorio
+
+
+
+
+}
